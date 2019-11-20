3a6b4d80cd20a0dcaf58b5dc168d4d2010361b3e
Yasindu <tyasindu@gmail.com>
2019-11-21 01:21:36 +0530

    Launch Screen done

---
+++ T.M.Y.S-Thennakoon-COBSCCOMP181P015/ViewController/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  T.M.Y.S-Thennakoon-COBSCCOMP181P015
+//
+//  Created by MacBook Air on 11/21/19.
+//  Copyright © 2019 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
